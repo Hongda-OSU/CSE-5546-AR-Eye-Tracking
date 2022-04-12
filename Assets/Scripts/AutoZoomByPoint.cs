142976d1e9875ce3dc291d8c86942a7214f6372d
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2022-04-11 23:03:20 -0400

    Small modification

--- Assets/Scripts/AutoZoomByPoint.cs
+++ Assets/Scripts/AutoZoomByPoint.cs
@@ -4,7 +4,8 @@ public class AutoZoomByPoint : MonoBehaviour
 {
     // Use [a,b] [c,d] value of the important portion in image
     // Calculate the center point
-    // Camera zoom in to the center point (FOV down)
+    // Change the center point position relative to screen position
+    // Move camera to the point
 
     public float zoomSpeed;
     public float minZoom;
@@ -15,16 +16,17 @@ public class AutoZoomByPoint : MonoBehaviour
         Zoom();
     }
 
-    void Zoom()
+    private void Zoom()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit point;
         Physics.Raycast(ray, out point, 1000);
+
+        // direction to the mouse position 
         Vector3 Scrolldirection = ray.GetPoint(5);
 
         float step = zoomSpeed * Time.deltaTime;
 
-        // Allows zooming in and out via the mouse wheel
         if (Input.GetAxis("Mouse ScrollWheel") > 0 && Scrolldirection.y > minZoom)
         {
             transform.position = Vector3.MoveTowards(transform.position, Scrolldirection, Input.GetAxis("Mouse ScrollWheel") * step);
@@ -34,5 +36,20 @@ public class AutoZoomByPoint : MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, Scrolldirection, Input.GetAxis("Mouse ScrollWheel") * step);
         }
     
+    }
+
+    private void RecoverImage()
+    {
+
+    }
+
+    private Vector2 CalculateFocusPoint()
+    {
+        return Vector2.one;
+    }
+
+    private Vector2 PointTransfer(Vector2 centerPoint)
+    {
+        return Vector2.one;
     }
 }
