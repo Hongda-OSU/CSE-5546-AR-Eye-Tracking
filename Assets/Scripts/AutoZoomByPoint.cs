b79c49ab38a2507c81f32629ca093260ebffca57
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2022-04-11 22:53:38 -0400

    Point zooming (not finished)

--- Assets/Scripts/AutoZoomByPoint.cs
+++ Assets/Scripts/AutoZoomByPoint.cs
@@ -6,13 +6,33 @@ public class AutoZoomByPoint : MonoBehaviour
     // Calculate the center point
     // Camera zoom in to the center point (FOV down)
 
-    void Start()
-    {
-        
-    }
+    public float zoomSpeed;
+    public float minZoom;
+    public float maxZoom;
 
     void Update()
     {
-        
+        Zoom();
+    }
+
+    void Zoom()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit point;
+        Physics.Raycast(ray, out point, 1000);
+        Vector3 Scrolldirection = ray.GetPoint(5);
+
+        float step = zoomSpeed * Time.deltaTime;
+
+        // Allows zooming in and out via the mouse wheel
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && Scrolldirection.y > minZoom)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Scrolldirection, Input.GetAxis("Mouse ScrollWheel") * step);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && Scrolldirection.y < maxZoom)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Scrolldirection, Input.GetAxis("Mouse ScrollWheel") * step);
+        }
+    
     }
 }
