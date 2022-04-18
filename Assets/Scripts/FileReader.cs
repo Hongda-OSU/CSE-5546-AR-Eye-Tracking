b6169e66bccfc13f555d2e62266365b405c047b6
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2022-04-18 18:19:00 -0400

    Finished

--- Assets/Scripts/FileReader.cs
+++ Assets/Scripts/FileReader.cs
@@ -39,7 +39,7 @@ public class FileReader : MonoBehaviour
     void Start()
     {
         // test writing data
-        StartCoroutine(WriteDataToFileForTest());
+        //StartCoroutine(WriteDataToFileForTest());
 
         // Read data from file every waitForSeconds
         StartCoroutine(ReadDataFromFile());
